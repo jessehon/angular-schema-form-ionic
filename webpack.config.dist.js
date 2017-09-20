9b281f53eebd38d8e317eedd7bd83727aab832e5
Jesse Hon <honkalong@gmail.com>
2017-09-20 12:56:31 +1000

    Replace 'bootstrap' with 'ionic' in webpack.config.dist output filenames

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -2,10 +2,10 @@ const config = require('./webpack.config.js');
 const path = require('path');
 
 config.entry = {
-  'angular-schema-form-bootstrap': [ path.join(__dirname, 'src', 'module') ],
-  'angular-schema-form-bootstrap-bundled': [ 'angular-schema-form', path.join(__dirname, 'src', 'module') ],
-  'angular-schema-form-bootstrap.min': [ path.join(__dirname, 'src', 'module') ],
-  'angular-schema-form-bootstrap-bundled.min': [ 'angular-schema-form', path.join(__dirname, 'src', 'module') ],
+  'angular-schema-form-ionic': [ path.join(__dirname, 'src', 'module') ],
+  'angular-schema-form-ionic-bundled': [ 'angular-schema-form', path.join(__dirname, 'src', 'module') ],
+  'angular-schema-form-ionic.min': [ path.join(__dirname, 'src', 'module') ],
+  'angular-schema-form-ionic-bundled.min': [ 'angular-schema-form', path.join(__dirname, 'src', 'module') ],
 }
 
 module.exports = config;
